a74548ad35b8e98bd86e456fa8b2f3674c6c9a25
Navya <vohrana@sheridancollege.ca>
2024-08-20 03:15:01 +0530

    Added OurDishesView view.

---
+++ LittleLemonMenuApp/View/OurDishesView.swift
@@ -0,0 +1,18 @@
+//
+//  OurDishesView.swift
+//  LittleLemonMenuApp
+//
+//  Created by Navya Vohra on 2024-08-20.
+//
+
+import SwiftUI
+
+struct OurDishesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OurDishesView()
+}
